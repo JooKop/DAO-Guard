fe43572daa760d222ab3b2493db3c672de413c24
Joonas Köppä <joonas.koppa@smartly.io>
2022-11-05 19:44:58 +0200

    More DAO guard contracts

--- Contracts/Social.sol
+++ Contracts/Social.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Health {
+contract Social {
 
     address public creator; //Creator of the contract
-    mapping(address => bool) public entitiesHealth; //Mapping of entities and their existence credibility based on health
+    mapping(address => bool) public entitiesSocial; //Mapping of entities and their existence credibility based on social actions
 
     //Store information about the creator of the Smart Contract on constructor
     constructor() {
         creator = msg.sender;
     }
 
-    //Add a new entity to the health credibility contract. So far only the creator can add new checks.
+    //Add a new entity to the social credibility contract. So far only the creator can add new checks.
     function addEntity(address addr, bool pass) public {
         require(msg.sender == creator); //Make sure the adder is the creator of the contract
-        entitiesHealth[addr] = pass; //Set the health credibility of a specific entity
+        entitiesSocial[addr] = pass; //Set the social credibility of a specific entity
     }
 }
